003ffec554c786c6c9093dfa428c027b4a184769
ely-as <mail@ely.as>
2023-04-20 15:59:01 -0700

    Refactor JS tests to make DOM tests easier to setup/teardown

--- js_tests/helpers.ts
+++ js_tests/helpers.ts
@@ -1,6 +1,21 @@
 import { readFileSync } from "fs";
+import { JSDOM } from "jsdom"
 import { resolve } from "path";
 
-export function loadText(fp: string) : string {
+function loadText(fp: string): string {
   return readFileSync(resolve(__dirname, fp), "utf-8");
+}
+
+export function loadDOMFromFile(fp: string) {
+  return new Promise(resolve => {
+    const { window } = new JSDOM(loadText(fp)) as unknown as Window;
+    global.window = window;
+    global.document = window.document;
+    window.addEventListener("load", resolve);
+  });
+}
+
+export function cleanDOM() {
+  delete global.window;
+  delete global.document;
 }
--- js_tests/main.spec.ts
+++ js_tests/main.spec.ts
@@ -1,14 +1,18 @@
 import { equal } from "assert";
-import { JSDOM } from "jsdom"
 
-import { loadText } from "./helpers";
 import { setup } from "../js_src/main";
 
-const { window } = new JSDOM(loadText("test.html")) as unknown as Window;
+import { cleanDOM, loadDOMFromFile } from "./helpers";
 
 describe("Test main module", () => {
+  before(async () => {
+    await loadDOMFromFile("test.html");
+  });
   it("setup() can add 'django' API entry point to DOM window object", () => {
     setup(window);
     equal(typeof window.django, "object");
+  });
+  after(async () => {
+    await cleanDOM();
   });
 });
