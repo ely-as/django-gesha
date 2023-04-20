ef2dde705dde86e4524fa73613882134abda9dfc
ely-as <mail@ely.as>
2023-04-20 00:06:22 -0700

    Add and apply ESLint rules: 2-space indent, double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double"]
+  },
   root: true
 };
--- js_src/main.ts
+++ js_src/main.ts
@@ -1,14 +1,14 @@
 // Single point of entry `django` for browser API
 declare global {
-    interface Window {
-        django: object;
-    }
+  interface Window {
+    django: object;
+  }
 }
 
 export function setup(w: Window) {
-    w.django = w.django || {};
+  w.django = w.django || {};
 }
 
 if (typeof window !== "undefined") {
-    setup(window);
+  setup(window);
 }
