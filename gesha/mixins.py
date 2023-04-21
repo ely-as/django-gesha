73974a81c8653108230f46cb4669b7361e58d047
ely-as <mail@ely.as>
2023-04-20 17:07:32 -0700

    Ignore misc type error in JSContextMixin super method call (#2)

--- gesha/mixins.py
+++ gesha/mixins.py
@@ -8,7 +8,7 @@ class JSContextMixin:
         return {}
 
     def get_context_data(self, **kwargs) -> typing.Dict:
-        context = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)  # type: ignore[misc]
         js_context_key = get_js_context_key()
         context[js_context_key] = self.get_js_context_data(**kwargs)
         return context
