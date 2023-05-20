6b28ac2923d1b5132d55f6acd010802c95a34e05
ely-as <mail@ely.as>
2023-05-19 22:04:33 -0700

    Release v0.1.alpha3

--- gesha/__init__.py
+++ gesha/__init__.py
@@ -2,7 +2,7 @@ from gesha.mixins import JSContextMixin
 from gesha.views import create_js_context_data
 
 # see https://peps.python.org/pep-0440/
-__version__ = "0.1.alpha2"
+__version__ = "0.1.alpha3"
 
 __all__ = [
     "JSContextMixin",
