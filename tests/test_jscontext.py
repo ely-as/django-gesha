3b62197e3069f37527465af989ff1d10eeb21db4
ely-as <mail@ely.as>
2023-05-19 20:24:45 -0700

    Parameterize all jscontext tests with different view types
    i.e. class-based, func-based and async views

--- tests/test_jscontext.py
+++ tests/test_jscontext.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+from collections.abc import Iterator
 
 import django
 import pytest
@@ -18,13 +19,17 @@ paths_to_test_for_presence_of_valid_script_html: list[str] = [
     "/func-based/",  # tests create_js_context_data()
 ]
 
-if django.VERSION[0] >= 4 or (django.VERSION[0] == 3 and django.VERSION[1] >= 1):
+if django.VERSION[:2] >= (3, 1):
     paths_to_test_for_presence_of_valid_script_html += [
         "/async/",  # tests async
     ]
 
 
-@pytest.mark.parametrize("path", paths_to_test_for_presence_of_valid_script_html)
+@pytest.fixture(params=paths_to_test_for_presence_of_valid_script_html)
+def path(request: pytest.FixtureRequest) -> Iterator[str]:
+    yield request.param
+
+
 def test_jscontext_tag_generates_script_html(path: str) -> None:
     soup = request_soup("GET", path)
     json_script = soup.find("script", id="js_context_data")
@@ -33,24 +38,28 @@ def test_jscontext_tag_generates_script_html(path: str) -> None:
     assert "myString" in js_context
 
 
-def test_jscontext_tag_works_when_setting_GESHA_JSCONTEXT_KEY_is_changed() -> None:
+def test_jscontext_tag_works_when_setting_GESHA_JSCONTEXT_KEY_is_changed(
+    path: str,
+) -> None:
     with override_settings(GESHA_JSCONTEXT_KEY="different_key"):
-        soup = request_soup("GET", "/")
+        soup = request_soup("GET", path)
     json_script = soup.find("script", id="different_key")
     assert json_script
 
 
-def test_paths_are_empty_when_GESHA_ALLOWED_URL_PATTERNS_is_empty() -> None:
+def test_paths_are_empty_when_GESHA_ALLOWED_URL_PATTERNS_is_empty_list(
+    path: str,
+) -> None:
     with override_settings(GESHA_ALLOWED_URL_PATTERNS=[]):
-        soup = request_soup("GET", "/")
+        soup = request_soup("GET", path)
     json_script = soup.find("script", id="js_context_data")
     js_context = json.loads(json_script.text)  # type: ignore[union-attr]
     assert js_context["_gesha"]["paths"] == {}
 
 
-def test_paths_are_filtered_when_GESHA_ALLOWED_URL_PATTERNS_is_set() -> None:
+def test_paths_are_filtered_when_GESHA_ALLOWED_URL_PATTERNS_is_set(path: str) -> None:
     with override_settings(GESHA_ALLOWED_URL_PATTERNS=["fake:test"]):
-        soup = request_soup("GET", "/")
+        soup = request_soup("GET", path)
     json_script = soup.find("script", id="js_context_data")
     js_context = json.loads(json_script.text)  # type: ignore[union-attr]
     assert len(js_context["_gesha"]["paths"]) == 1
